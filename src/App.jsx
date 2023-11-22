a8432f71c1454b7d87f2511bedd83fcec18e9ec6
swathireddappa <swathireddy1133@gmail.com>
2023-11-22 18:19:10 +0100

    add the icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 
 import React, { useState } from "react";
+import { Icon } from 'react-icons-kit';
+import { arrows_circle_check } from "react-icons-kit/linea/arrows_circle_check";
+import { arrows_exclamation } from "react-icons-kit/linea/arrows_exclamation";
+
 
 const  FormValidation =  ()=> {
  
@@ -20,6 +24,7 @@ const  FormValidation =  ()=> {
         ...Data, [name] : value
      })
   }
+  // Form Validation
 
   const HandleSubmit = (e) => {
     e.preventDefault()
@@ -45,12 +50,64 @@ const  FormValidation =  ()=> {
       validationErrors.confirmpassword = "password not matched.."
     }
   
-
-  setErrors(validationErrors)
-   if(Object.keys(validationErrors) .length === 0){
-    alert("Thanks YOur Form is Submitted Successfully")
+    setErrors(validationErrors)
+   if(Object.keys(validationErrors).length === 0){
+    alert("Thanks Your Form is Submitted Successfully")
    }
   }
+   
+  // Password Validation
+  const [Lowervalidate, setLowervalidate] = useState(false);
+  const [Uppervalidate, setUppervalidate] = useState(false);
+  const [Numbervalidate, setNumbervalidate] = useState(false);
+  const [SpecialCharactervalidate, setSpecialCharactervalidate] = useState(false);
+  const [Lengthvalidate, setLengthvalidate] = useState(false);
+
+  const pswrdchange = (value) => {
+    // regex
+    const Lower = new RegExp('(?=.*[a-z])');
+    const Upper = new RegExp('(?=.*[A-Z])');
+    const Number = new RegExp('(?=.*[0-9])');
+    const Specialcharacter = new RegExp('(?=.*[@,$,&])');
+    const Length = new RegExp('(?=.*{<8})');
+ 
+    // Lowercase validation
+    if(Lower.test(value)){
+      setLowervalidate(true);
+    }
+    else{setLowervalidate(false);
+    }
+
+    // Uppercase Validation
+    if(Upper.test(value)){
+      setUppervalidate(true);
+    }
+    else{setUppervalidate(false);
+    }
+
+    // Number Validation
+    if(Number.test(value)){
+      setNumbervalidate(true);
+    }
+    else{setNumbervalidate(false);
+    } 
+
+    // SpecialCharacter Validation
+    if(Specialcharacter.test(value)){
+      setSpecialCharactervalidate(true);
+    }
+    else{setSpecialCharactervalidate(false);
+    }
+
+    // Length Validation
+    if(Length.test(value)){
+      setLengthvalidate(true);
+    }
+    else{setLengthvalidate(false);
+    }
+  }
+
+
 
   return (
 
@@ -72,14 +129,80 @@ const  FormValidation =  ()=> {
 
     <div>
       <label > Password:</label>
-      <input type="password" name="password" placeholder="*****" onChange={handleChange} />
+      <input type="password" name="password" placeholder="*****" onChange={pswrdchange} />
       {Errors.password && <span>{Errors.password}</span>}
     </div>
 
     {/* validation Tracker */}
+      <div>
+          <div className={Lowervalidate?'validate':'not-validate'}>
+           {Lowervalidate?(
+            <span className="list-icon">
+              <Icon icon={arrows_circle_check} size={18} />
+            </span>
+            ):(
+              <span className='list-icon'>
+              <Icon icon={arrows_exclamation} />
+            </span>
+            )}
+
+          At least one Lower letter
+          </div>
+
+        <div  className={Uppervalidate?'validate':'not-validate'}>
+        {Uppervalidate?(
+            <span className="list-icon">
+              <Icon icon={arrows_circle_check} size={18} />
+            </span>
+            ):(
+              <span className='list-icon'>
+              <Icon icon={arrows_exclamation} />
+            </span>
+            )}
+          At least one Upper letter
+          </div>
+
+        <div className={SpecialCharactervalidate?'validate':'not-validate'}>
+        {SpecialCharactervalidate?(
+            <span className="list-icon">
+              <Icon icon={arrows_circle_check} size={18} />
+            </span>
+            ):(
+              <span className='list-icon'>
+              <Icon icon={arrows_exclamation} />
+            </span>
+            )}
+          At least one Special Character
+          </div>
+
+        <div className={Numbervalidate?'validate':'not-validate'}>
+        {Numbervalidate?(
+            <span className="list-icon">
+              <Icon icon={arrows_circle_check} size={18} />
+            </span>
+            ):(
+              <span className='list-icon'>
+              <Icon icon={arrows_exclamation} />
+            </span>
+            )}
+          At least one Number
+          </div>
+
+        <div className={Lengthvalidate?'validate':'not-validate'}>
+        {Lengthvalidate?(
+            <span className="list-icon">
+              <Icon icon={arrows_circle_check} size={18} />
+            </span>
+            ):(
+              <span className='list-icon'>
+              <Icon icon={arrows_exclamation} />
+            </span>
+            )}
+
+          At least 8 Character</div>
+       </div>
 
 
-    
 
     <div>
        <label >Confirm Password:</label>
@@ -91,6 +214,7 @@ const  FormValidation =  ()=> {
 
     </div>
     </form>
+    
   );
 }
 
